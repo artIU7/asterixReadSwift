df7b6e903603702b78eb66ef4d6118c49cfbdd41
Артем Стратиенко <stratienko.a@macbook-pro-artem.local>
2017-06-22 18:45:47 +0300

    adding class checkDirectory()

---
+++ readMessageAsterix021/classCheckDirectory.swift
@@ -0,0 +1,9 @@
+//
+//  classCheckDirectory.swift
+//  readMessageAsterix021
+//
+//  Created by Артем Стратиенко on 22.06.17.
+//  Copyright © 2017 Артем Стратиенко. All rights reserved.
+//
+
+import Foundation
