52c94962fe9455ee738161813de42c6a2be53f43
Артем Стратиенко <stratienko.a@macbook-pro-artem.local>
2017-06-22 19:38:10 +0300

    add others

--- readMessageAsterix021/ViewController.swift
+++ readMessageAsterix021/ViewController.swift
@@ -12,9 +12,28 @@ class ViewController: NSViewController {
     
     @IBOutlet var ReadScroll: NSClipView!
     
-    
+   // Action for readMess
     @IBAction func readMess021(_ sender: Any) {
+        
+        // Читаем
+        let fileManager = FileManager()
+        let dirWFile = "/Users/stratienko.a/Desktop/asterixFile"
+        let fileName = "amber104.asterix"
+        let fromCheck = CheckDirectoryAsterix(fileManager: fileManager, dirWFile: dirWFile, fileName: fileName)
+        let directoryWithFiles = fromCheck.checkDirectory() ?? "Empty"
+        let path = (fromCheck.dirWFile as NSString).appendingPathComponent(directoryWithFiles)
+        do {
+            let contentsOfFile = try NSString(contentsOfFile: path, encoding: String.Encoding.utf8.rawValue)
+            print("content of the file is: \(contentsOfFile)")
+        } catch let error as NSError {
+            print("there is an file reading error: \(error)")
+        }
+        //
+        let sHex = CheckDirectoryAsterix(fileManager: fileManager, dirWFile: dirWFile, fileName: fileName)
+        sHex.dataWithHexString(hex: "68656c6c6f2c20776f726c64")
+        //
     }
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- readMessageAsterix021/classCheckDirectory.swift
+++ readMessageAsterix021/classCheckDirectory.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+class CheckDirectoryAsterix{
+    
+        var fileManager = FileManager()
+        var dirWFile = ""
+        var fileName = ""
+ // func 1
+        func checkDirectory() -> String? {
+            do
+            {
+                let filesInDirectory = try fileManager.contentsOfDirectory(atPath: dirWFile)
+                let files = filesInDirectory
+                if files.count > 0 {
+                    if files.first == fileName { print("\(fileName) found")
+                        return files.first
+                    } else {
+                        print("File not found")
+                        return nil
+                    }
+                }
+            }
+            catch let error as NSError  {
+                
+                print(error)
+            }
+            return nil
+    }
+    // func char analyz and conv To Data()
+    func dataWithHexString(hex: String) -> Data {
+        var hex = hex
+        var data = Data()
+        while(hex.characters.count > 0) {
+            let c: String = hex.substring(to: hex.index(hex.startIndex, offsetBy: 2))
+            hex = hex.substring(from: hex.index(hex.startIndex, offsetBy: 2))
+            var ch: UInt32 = 0
+            Scanner(string: c).scanHexInt32(&ch)
+            var char = UInt8(ch)
+            data.append(&char, count: 1)
+        }
+        return data
+    }
+
+// create  init(f,d,fn)
+
+    init(fileManager: FileManager, dirWFile: String, fileName: String){
+        self.fileManager = fileManager
+        self.dirWFile = dirWFile
+        self.fileName = fileName
+
+    }
+
+}
