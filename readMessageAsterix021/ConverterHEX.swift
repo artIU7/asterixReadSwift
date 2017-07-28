28ab4b00493c50c835a9edcb0489876f23623138
Артем Стратиенко <stratienko.a@macbook-pro-artem.local>
2017-07-28 17:17:04 +0300

    edit hexConv!!!

--- readMessageAsterix021/ConverterHEX.swift
+++ readMessageAsterix021/ConverterHEX.swift
@@ -25,42 +25,42 @@ class hexConvertDec: isHexString{
         self.charHex = charHex
 
     }
-    func dCh(charL : String) -> UInt8
+    func dCh(charL : UInt8) -> String
     {
-        if charL == "0" {
-            return 0
-        } else if charL == "1"{
-            return 1
-        } else if charL == "2"{
-            return 2
-        } else if charL == "3"{
-            return 3
-        } else if charL == "4"{
-            return 4
-        } else if charL == "5"{
-            return 5
-        } else if charL == "6"{
-            return 6
-        } else if charL == "7"{
-            return 7
-        } else if charL == "8"{
-            return 8
-        } else if charL == "9"{
-            return 9
-        } else if charL == "A"{
-            return 10
-        } else if charL == "B"{
-            return 11
-        } else if charL == "C"{
-            return 12
-        } else if charL == "D"{
-            return 13
-        } else if charL == "E"{
-            return 14
-        } else if charL == "F"{
-            return 15
+        if charL == 0 {
+            return "0"
+        } else if charL == 1{
+            return "1"
+        } else if charL == 2{
+            return "2"
+        } else if charL == 3{
+            return "3"
+        } else if charL == 4{
+            return "4"
+        } else if charL == 5{
+            return "5"
+        } else if charL == 6{
+            return "6"
+        } else if charL == 7{
+            return "7"
+        } else if charL == 8{
+            return "8"
+        } else if charL == 9{
+            return "9"
+        } else if charL == 10{
+            return "A"
+        } else if charL == 11{
+            return "B"
+        } else if charL == 12{
+            return "C"
+        } else if charL == 13{
+            return "D"
+        } else if charL == 14{
+            return "E"
+        } else if charL == 15{
+            return "F"
         }
-        return 16
+        return ""
     }
     
     func DecHexTo(charHex: UInt8) -> UInt8 {
@@ -69,7 +69,11 @@ class hexConvertDec: isHexString{
         var result = [UInt8]()
         buf = ""
         let sSysHex : UInt8 = 16
-       
+        // 1 var - N10
+        // 2 var - kSystem Letter
+        // 3 var - bufresult
+        // 4 var - result
+        if (charHex >= sSysHex){
         for ini in result
         {
             var i : Int
@@ -83,10 +87,16 @@ class hexConvertDec: isHexString{
             result[i] = charHex%sSysHex
             
         }
+        } else
+        {
+             dCh(charL: charHex)
+        }
         var nValue : UInt8
         nValue = 0
+       
+        
         for ini in result{
-        var i : Int = 0
+        let i : Int = 0
             
             nValue = result[i]+nValue
         }
